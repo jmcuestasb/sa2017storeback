7ea141948438393636e6cb547936116764328cbe
jmcuestasb <jmcuestasb@unal.edu.co>
2017-11-08 23:19:27 -0500

    commit2

--- start2.sh
+++ start2.sh
@@ -1,7 +1,7 @@
 ./rancher-compose --project-name contapp \
     --url http://192.168.99.100:8080/v1/projects/1a5 \
-    --access-key 970782B50D265F61035A \
-    --secret-key gdjSAuMHcrRms8oT6tRYy4UyMqn46ZNzXnCY7yKD \
+    --access-key 3965148E84E5A100D755 \
+    --secret-key Jye5JjsNid8ZhJwGswsufjpVh9ExrW9XsKryyQra \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
